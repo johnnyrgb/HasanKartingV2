9f3fd1f166adadb0818873f3ed894972e1e9e014
Ivan <vaneklopatin.main@gmail.com>
2024-05-06 17:28:39 +0300

    loginObject, registrationObject созданы

---
+++ HasanKarting/web/src/components/entities/loginObject.ts
@@ -0,0 +1,8 @@
+interface loginObject
+{
+    username: string,
+    password: string,
+    rememberMe: boolean
+}
+
+export default loginObject;
---
+++ HasanKarting/web/src/components/entities/registrationObject.ts
@@ -0,0 +1,9 @@
+interface registrationObject
+{
+    email: string,
+    password: string,
+    passwordConfirmation: string,
+    role: string
+}
+
+export default registrationObject;
